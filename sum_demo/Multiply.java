ee7afe54f3161848da5117c4e12599cdd8eaa464
Vipin Regulwar <vipinrglwr271@gmail.com>
2022-01-12 19:21:58 +0530

    java

---
+++ sum_demo/Multiply.java
@@ -0,0 +1,10 @@
+package sum_demo;
+
+public class Multiply {
+  public class Sum {
+    public static void main(String[] args) {
+      int sum = 45+ 11;
+      System.out.println(sum);
+    }
+  }
+}
